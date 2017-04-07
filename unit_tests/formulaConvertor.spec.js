178ec9610f07f1b602320274ac2bf9fe41a088fe
nazar <pyvovarnazar@gmail.com>
2017-04-08 00:02:14 +0300

    realized and tested exponentiation parsing in 3 langs

--- unit_tests/formulaConvertor.spec.js
+++ unit_tests/formulaConvertor.spec.js
@@ -2,13 +2,7 @@ const expect = require('chai').expect;
 
 const formulaConverter = require('../lib/formulaConverter');
 
-describe('translatePascalToClassic', () => {
-
-    it('should return x<sup\>2</sup> to x^2', () => {
-        const actual = formulaConverter.translatePascalToClassic('x^2');
-        expect(actual).to.equal('x<sup\>2</sup>');
-    });
-
+describe('translate', ()=> {
     it('should return error "count of parentheses are not even" to (x', () => {
         const actual = formulaConverter.translatePascalToClassic('(x');
         expect(actual).to.deep.equal({
@@ -28,6 +22,33 @@ describe('translatePascalToClassic', () => {
         expect(actual).to.deep.equal({
             error: 'count of square brackets are not even'
         });
+    });
+});
+
+describe('translatePascalToClassic', () => {
+
+    it('should return x<sup\>2</sup> to x^2', () => {
+        const actual = formulaConverter.translatePascalToClassic('x^2');
+        expect(actual).to.equal('x<sup\>2</sup>');
+    });
+
+});
+
+
+describe('translateCtoClassic', () => {
+
+    it('should return x<sup\>2</sup> to math.pow(x,2)', () => {
+        const actual = formulaConverter.translateCtoClassic('math.pow(x,2)');
+        expect(actual).to.equal('x<sup\>2</sup>');
+    });
+
+});
+
+describe('translateFortranToClassic', () => {
+
+    it('should return x<sup\>2</sup> to math.pow(x,2)', () => {
+        const actual = formulaConverter.translateFortranToClassic('x**2');
+        expect(actual).to.equal('x<sup\>2</sup>');
     });
 
 });
