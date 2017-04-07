782a478745f5aff726fb6fef33aea1e3c62bb3b2
loginkr <shvajka@gmail.com>
2017-04-08 02:12:12 +0300

    formula xml

--- public/javascripts/Editor.jsx
+++ public/javascripts/Editor.jsx
@@ -2,6 +2,7 @@ const React = require('react');
 const axios = require('axios');
 const renderHTML = require('react-render-html');
 
+
 const Editor = React.createClass({
     getInitialState(){
         return {
@@ -38,7 +39,7 @@ const Editor = React.createClass({
                 this.setState(state);
             })
     },
-    SaveToImg() {
+    saveToImg() {
         html2canvas($("#output"), {
             onrendered: function(canvas) {
                 document.location.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
@@ -46,22 +47,54 @@ const Editor = React.createClass({
             }
         });
     },
+    saveToXml(){
+        var xmlFile="<?xml version=\"1.0\" encoding=\"UTF-8\"?> <formula>"+this.state.outputFormula+"</formula>";
+        var blob = new Blob([xmlFile], {
+            type: "text/plain;charset=utf-8"
+        });
+
+        saveAs(blob, "RAW.xml");
+    },
 
     render() {
         return(
             <div>
-                {this.state.error}
-                <textarea value={this.state.inputFormula}
-                          placeholder="input"
-                          onChange={this.onInputFormulaChange}
-                >
-                </textarea>
+                <div className="form-group">
+                    <label>Formula:</label>
+                    <textarea className="form-control"
+                              value={this.state.inputFormula}
+                              placeholder="input"
+                              onChange={this.onInputFormulaChange}
+                    >
+                    </textarea>
+                    {this.state.error}
+                </div>
 
-                <span id="output">
-                    {renderHTML(this.state.outputFormula)}
-                </span>
-                <button onClick={this.SaveToImg}>Save Formula to Image</button>
-                <button onClick={this.processInputFormula}>Process</button>
+                <div className="form-group">
+                    <label>Output:</label>
+                    <span id="output">
+                        {renderHTML(this.state.outputFormula)}
+                    </span>
+                </div>
+                <br/>
+                <div className="form-group">
+                    <label>Select language:</label>
+                    <select className="form-control" id="select_lang">
+                        <option value="">1</option>
+                        <option value="">2</option>
+                        <option value="">3</option>
+                        <option value="">4</option>
+                    </select>
+                </div>
+
+                <br/>
+                <button className="btn btn-success" onClick={this.processInputFormula}>Process</button>
+                <br/>
+                <br/>
+                <button className="btn btn-default" onClick={this.saveToImg}>Save Formula to Image</button>
+                <br/>
+                <br/>
+                <button className="btn btn-default" onClick={this.saveToXml}>Save Formula to Xml</button>
             </div>
         )
     }
